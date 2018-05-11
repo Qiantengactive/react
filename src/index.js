0a9f5e43a765fb3174b56c070ac7876113e45ea1
huolinag <936129056@qq.com>
2018-05-11 20:55:42 +0800

    add menu

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-// import { HashRouter as Router, Route, Link,Switch } from 'react-router-dom';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import ReactDOM from 'react-dom'; //渲染到dom
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<app/>, document.getElementById('root'));
+/* 页面 */
+import Home from './page/home/index.jsx';
+// import Home from 'page/home/index.jsx';
+import Layout from './component/layout/index';
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Layout>
+          <Switch>
+            {/*没有switch 路劲是一个个匹配  Switch只匹配他第一个匹配的东西*/}
+            <Route exact path="/" component={Home} />
+            <Redirect from="*" to="/" />
+          </Switch>
+        </Layout>
+      </Router>
+    );
+  }
+}
+ReactDOM.render(<App />, document.getElementById('root'));
 // ReactDOM.render(App, document.getElementById('root'))
 registerServiceWorker();
